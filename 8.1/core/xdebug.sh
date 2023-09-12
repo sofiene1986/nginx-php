8932ebb20f5b6ce31c5fd009c99406632b3ed17f
sofiene1986 <oasisdev1986>
2023-09-12 15:30:18 +0100

    correction dockerfile

--- 8.1/core/xdebug.sh
+++ 8.1/core/xdebug.sh
@@ -5,12 +5,12 @@ case $status in
   on)
       sudo sed -i 's/^;*\(.*xdebug\.so\)/\1/' /usr/local/etc/php/php.ini
       echo "xdebug on"
-      sudo service apache2 reload
+      sudo service nginx reload
       ;;
   off)
       sudo sed -i 's/^\(.*xdebug\.so\)/;\1/' /usr/local/etc/php/php.ini
       echo "xdebug off"
-      sudo service apache2 reload
+      sudo service nginx reload
       ;;
     *)
       echo "usage xdebug: on|off" 1>&2;
