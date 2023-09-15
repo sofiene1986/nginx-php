6289d6dcc575469a1010a8e67c9de61d25694abb
sofiene1986 <oasisdev1986>
2023-09-15 18:32:38 +0100

    correction dockerfile

--- 8.1/config/container/docker-entrypoint.sh
+++ 8.1/config/container/docker-entrypoint.sh
@@ -10,6 +10,26 @@ if [ -n "$DOCUMENTROOT" ]; then
 else
  documentRoot="\/var\/www\/html"
 fi
+case $USE_XDEBUG in
+  on)
+      sudo sed -i 's/^;*\(.*xdebug\.so\)/\1/' /usr/local/etc/php/php.ini
+      ;;
+  off)
+      sudo sed -i 's/^\(.*xdebug\.so\)/;\1/' /usr/local/etc/php/php.ini
+      ;;
+    *)
+      sudo sed -i 's/^\(.*xdebug\.so\)/;\1/' /usr/local/etc/php/php.ini
+esac
+case $USE_XHPROF in
+  on)
+      sudo sed -i 's/^;*\(.*tideways_xhprof\.so\)/\1/' /usr/local/etc/php/php.ini
+      ;;
+  off)
+      sudo sed -i 's/^\(.*tideways_xhprof\.so\)/;\1/' /usr/local/etc/php/php.ini
+      ;;
+    *)
+      sudo sed -i 's/^\(.*tideways_xhprof\.so\)/;\1/' /usr/local/etc/php/php.ini
+esac
 sudo  sed -i "s/DOCUMENTROOT/$documentRoot/g" /etc/nginx/nginx.conf
 service nginx start && php-fpm
 exec "$@"
